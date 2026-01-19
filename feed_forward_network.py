820bf24891bfafb58cf7e3b9440af7c16e847e23
yuanzh89 <yuanzh89@gmail.com>
2026-01-20 00:58:33 +0800

    Update feed_forward_network.py

--- feed_forward_network.py
+++ feed_forward_network.py
@@ -6,17 +6,20 @@ class FeedForwardNetwork(nn.Module):
         super().__init__()
 
         self.layer_norm = nn.LayerNorm(d_model)
-        self.up_proj = nn.Linear(d_model, d_ff)
+        self.up_proj = nn.Linear(d_model, d_ff, bias=False)
         self.gelu = nn.GELU()
-        self.down_proj = nn.Linear(d_ff, d_model)
-        self.dropout = None if dropout == 0.0 else nn.Dropout(dropout)
+        self.down_proj = nn.Linear(d_ff, d_model, bias=False)
+        self.dropout = nn.Dropout(p=dropout) if dropout > 0.0 else nn.Identity()
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
         residual = input
         x = self.layer_norm(input)
+
         x = self.up_proj(x)
         x = self.gelu(x)
         x = self.down_proj(x)
+
         x = self.dropout(x)
+
         return x + residual
 
