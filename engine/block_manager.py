ef3934452091739cf0e0d99342541c044f2deb59
yuanzh89 <yuanzh89@gmail.com>
2026-02-21 17:11:26 -0800

    block_manager.py

--- engine/block_manager.py
+++ engine/block_manager.py
@@ -1,6 +1,7 @@
 from collections import deque
 
 import numpy as np
+import torch
 import xxhash
 
 from sequence import Sequence
@@ -11,11 +12,23 @@ class Block:
     A block of physical KV cache can be referred by multiple sequences at the same time, recorded by ref_count.
     """
 
-    def __init__(self, block_id: int):
+    def __init__(self, block_id: int, block_size: int, dtype: torch.dtype) -> None:
         self.block_id = block_id
+        self.block_size = block_size
+        self.dtype = dtype
+
+        self.k_cache = torch.empty(block_size, dtype=dtype)
+        self.v_cache = torch.empty(block_size, dtype=dtype)
+
         self.ref_count = 0
         self.hash = -1
         self.token_ids = []
+
+    def __len__(self) -> int:
+        return len(self.token_ids)
+
+    def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor]:
+        return self.k_cache[idx], self.v_cache[idx]
 
     def update(self, hash: int, token_ids: list[int]) -> None:
         self.hash = hash
