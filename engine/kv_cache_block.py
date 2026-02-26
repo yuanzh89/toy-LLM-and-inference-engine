091638b0f54615b3a4680ac23a65a8e616c86626
yuanzh89 <yuanzh89@gmail.com>
2026-02-25 22:16:36 -0800

    naive_block_manager.py

--- engine/kv_cache_block.py
+++ engine/kv_cache_block.py
@@ -3,32 +3,28 @@ import torch
 
 class Block:
     """
-    A block of physical KV cache can be referred by multiple sequences at the same time, recorded by ref_count.
+    A block of physical KV cache can be referred by multiple sequences at the same time, recorded by _ref_count.
+    Serving just one layers
     """
 
-    def __init__(self, block_id: int, block_size: int, dtype: torch.dtype) -> None:
+    def __init__(self, block_id: int, layer_id: int, token_ids: list[int], k_cache: torch.Tensor = None,
+                 v_cache: torch.Tensor = None, max_token_size_per_kv_cache_block: int = 16):
         self.block_id = block_id
-        self.block_size = block_size
-        self.dtype = dtype
-
-        self.k_cache = torch.empty(block_size, dtype=dtype)
-        self.v_cache = torch.empty(block_size, dtype=dtype)
-
-        self.ref_count = 0
-        self.hash = -1
-        self.token_ids = []
+        self.layer_id = layer_id
+        self.token_ids = token_ids
+        self.max_token_size_per_kv_cache_block = max_token_size_per_kv_cache_block
+        self.ref_count = 1
+        self.k_cache = k_cache
+        self.v_cache = v_cache
 
     def __len__(self) -> int:
         return len(self.token_ids)
 
-    def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor]:
-        return self.k_cache[idx], self.v_cache[idx]
+    @property
+    def hash(self) -> int:
+        return hash(tuple(self.token_ids))
 
-    def update(self, hash: int, token_ids: list[int]) -> None:
-        self.hash = hash
-        self.token_ids = token_ids
-
-    def reset(self):
-        self.ref_count = 1
-        self.hash = -1
-        self.token_ids = []
+    def append(self, token_id: int, k: torch.Tensor, v: torch.Tensor):
+        self.token_ids.append(token_id)
+        self.k_cache = torch.cat([self.k_cache, k], dim=0)
+        self.v_cache = torch.cat([self.v_cache, v], dim=0)
